82e0eb362907006a40c8ec9b6b8abbbe97dbfbfd
hrpzcf <pphrp@qq.com>
2023-02-26 16:14:15 +0800

    更新依赖 BouncyCastle.Cryptography 版本（1.9.0 -> 2.1.1，此依赖只用于 sha224 计算）

--- HashCalculator/AppLoading.cs
+++ HashCalculator/AppLoading.cs
@@ -30,7 +30,7 @@ namespace HashCalculator
         private static Assembly ApplicationAssemblyResolve(object sender, ResolveEventArgs arg)
         {
             string asmbName = new AssemblyName(arg.Name).Name;
-            if (!(asmbName == "BouncyCastle.Crypto" ||
+            if (!(asmbName == "BouncyCastle.Cryptography" ||
                 asmbName == "Microsoft.WindowsAPICodePack.Shell" ||
                 asmbName == "Microsoft.WindowsAPICodePack"))
                 return default;
