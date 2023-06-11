0759c98f72b7e64f7657c16d461407e5733c4cb9
hrpzcf <pphrp@qq.com>
2023-06-11 15:11:44 +0800

    主窗口增加显示：列表项目总数、待计算队列总数、待计算队列已完成数

--- HashCalculator/Others/Converters.cs
+++ HashCalculator/Others/Converters.cs
@@ -716,4 +716,23 @@ namespace HashCalculator
             throw new NotImplementedException();
         }
     }
+
+    internal class TotalProgressVisiblityCvt : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((QueueState)value)
+            {
+                case QueueState.Started:
+                    return Visibility.Visible;
+                default:
+                    return Visibility.Hidden;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
