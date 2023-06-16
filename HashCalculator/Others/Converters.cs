f301255e68d597ac735cc02c01a266f6624a7dba
hrpzcf <pphrp@qq.com>
2023-06-16 17:59:50 +0800

    "哈希值校验依据"输入框增加 PlaceHolder 类型的提示字符

--- HashCalculator/Others/Converters.cs
+++ HashCalculator/Others/Converters.cs
@@ -780,4 +780,24 @@ namespace HashCalculator
             throw new NotImplementedException();
         }
     }
+
+    internal class HashOrPathPlaceHolderVisibCvt : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!string.IsNullOrEmpty(value as string))
+            {
+                return Visibility.Hidden;
+            }
+            else
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
