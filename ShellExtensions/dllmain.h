0105c7726ee90f80c973776431def5c816a2f87f
hrpzcf <pphrp@qq.com>
2023-07-03 16:48:11 +0800

    创建 Shell 扩展动态库项目（未实现具体功能）

---
+++ ShellExtensions/dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h: 模块类的声明。
+
+class CShellExtensionsModule : public ATL::CAtlDllModuleT< CShellExtensionsModule >
+{
+public:
+	DECLARE_LIBID(LIBID_ShellExtensionsLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_SHELLEXTENSIONS, "{18d6b7f2-f466-481f-adfc-849b5f9fbd0b}")
+};
+
+extern class CShellExtensionsModule _AtlModule;
