dacc80e0f2220bd853fb4d070d078d603099062c
hrpzcf <pphrp@qq.com>
2023-09-17 17:11:13 +0800

    【搜索文件名】筛选器增加【忽略大小写】选项

---
+++ HashCalculator/HashFilters/StringComparer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashCalculator
+{
+    internal class StringComparer : IEqualityComparer<string>
+    {
+        public StringComparer() { }
+
+        public StringComparer(bool ignoreCase)
+        {
+            this.IgnoreCase = ignoreCase;
+        }
+
+        public bool IgnoreCase { get; }
+
+        public bool Equals(string x, string y)
+        {
+            if (this.IgnoreCase)
+            {
+                return x.Equals(y, StringComparison.OrdinalIgnoreCase);
+            }
+            else
+            {
+                return x.Equals(y);
+            }
+        }
+
+        public int GetHashCode(string str)
+        {
+            return this.IgnoreCase ? str.ToUpper().GetHashCode() : str.GetHashCode();
+        }
+    }
+}
