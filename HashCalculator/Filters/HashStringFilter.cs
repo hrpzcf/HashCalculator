f04c007fe2578d5c1498179f68c6ddc29f1508d5
hrpzcf <pphrp@qq.com>
2023-11-16 17:11:49 +0800

    原“哈希值”筛选器在用户输入错误的哈希值时所有文件都可以通过筛选，现在改为所有文件都不可以通过筛选

--- HashCalculator/Filters/HashStringFilter.cs
+++ HashCalculator/Filters/HashStringFilter.cs
@@ -30,48 +30,53 @@ namespace HashCalculator
             }
             HashSet<byte[]> expectedHashs = hashStrings.Select(i => CommonUtils.HashFromAnyString(i)).Where(
                 i => i != null).ToHashSet<byte[]>(BytesComparer.Default);
-            if (expectedHashs.Any())
+            if (!expectedHashs.Any())
             {
                 foreach (HashViewModel model in models)
                 {
-                    if (!model.Matched)
+                    model.Matched = false;
+                }
+                return;
+            }
+            foreach (HashViewModel model in models)
+            {
+                if (!model.Matched)
+                {
+                    continue;
+                }
+                if (model.AlgoInOutModels == null)
+                {
+                    model.Matched = false;
+                }
+                else
+                {
+                    HashSet<byte[]> modelHashs = model.AlgoInOutModels.Select(i => i.HashResult).ToHashSet<byte[]>(BytesComparer.Default);
+                    if (filterLogic == FilterLogic.Any)
                     {
-                        continue;
+                        if (!modelHashs.Overlaps(expectedHashs))
+                        {
+                            model.Matched = false;
+                        }
                     }
-                    if (model.AlgoInOutModels == null)
+                    else if (filterLogic == FilterLogic.Strict)
                     {
-                        model.Matched = false;
+                        if (!modelHashs.SetEquals(expectedHashs))
+                        {
+                            model.Matched = false;
+                        }
                     }
-                    else
+                    else if (filterLogic == FilterLogic.Within)
                     {
-                        HashSet<byte[]> modelHashs = model.AlgoInOutModels.Select(i => i.HashResult).ToHashSet<byte[]>(BytesComparer.Default);
-                        if (filterLogic == FilterLogic.Any)
+                        if (!modelHashs.IsSubsetOf(expectedHashs))
                         {
-                            if (!modelHashs.Overlaps(expectedHashs))
-                            {
-                                model.Matched = false;
-                            }
+                            model.Matched = false;
                         }
-                        else if (filterLogic == FilterLogic.Strict)
+                    }
+                    else if (filterLogic == FilterLogic.Cover)
+                    {
+                        if (!modelHashs.IsSupersetOf(expectedHashs))
                         {
-                            if (!modelHashs.SetEquals(expectedHashs))
-                            {
-                                model.Matched = false;
-                            }
-                        }
-                        else if (filterLogic == FilterLogic.Within)
-                        {
-                            if (!modelHashs.IsSubsetOf(expectedHashs))
-                            {
-                                model.Matched = false;
-                            }
-                        }
-                        else if (filterLogic == FilterLogic.Cover)
-                        {
-                            if (!modelHashs.IsSupersetOf(expectedHashs))
-                            {
-                                model.Matched = false;
-                            }
+                            model.Matched = false;
                         }
                     }
                 }
