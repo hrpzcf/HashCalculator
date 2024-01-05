5196bc7e2cdbd5b3a108af2012454f50bd30679a
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2024-01-05 16:32:47 +0800

    给【筛选与操作】面板添加一个用于指示正在筛选中的动态图标

--- HashCalculator/Others/Converters.cs
+++ HashCalculator/Others/Converters.cs
@@ -376,6 +376,26 @@ namespace HashCalculator
         }
     }
 
+    internal class BooleanTrueToVisibilityHiddenCvt : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool visible && visible)
+            {
+                return Visibility.Hidden;
+            }
+            else
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     internal class BooleanFlaseToVisibilityCollapsedCvt : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
