20ba93ccca6e340f0f456d379951bc718604723d
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2024-02-18 13:39:40 +0800

    调整【关于】窗口的开源项目列表的顺序和备注

--- HashCalculator/ViewModels/AboutWindowModel.cs
+++ HashCalculator/ViewModels/AboutWindowModel.cs
@@ -49,6 +49,46 @@ namespace HashCalculator
         public GenericItemModel[] OpenSourceProjects { get; } = new GenericItemModel[]
         {
             new GenericItemModel(
+                "BLAKE2",
+                "https://github.com/BLAKE2/BLAKE2",
+                "提供 BLAKE2 系列哈希算法的实现"),
+            new GenericItemModel(
+                "BLAKE3",
+                "https://github.com/BLAKE3-team/BLAKE3",
+                "提供 BLAKE3 系列哈希算法的实现"),
+            new GenericItemModel(
+                "CRC32",
+                "https://github.com/stbrumme/crc32",
+                "提供 CRC32 哈希算法的实现"),
+            new GenericItemModel(
+                "GmSSL",
+                "https://github.com/guanzhi/GmSSL",
+                "提供 SM3 哈希算法的实现"),
+            new GenericItemModel(
+                "OpenHashTab",
+                "https://github.com/namazso/OpenHashTab",
+                "提供 CRC64 哈希算法的实现"),
+            new GenericItemModel(
+                "QuickXorHash",
+                "https://github.com/namazso/QuickXorHash",
+                "提供 QuickXor 哈希算法的实现"),
+            new GenericItemModel(
+                "RHash",
+                "https://github.com/rhash/RHash",
+                "提供 eD2k/Has160/MD4/RipeMD160/SHA224/Whirlpool 算法的实现"),
+            new GenericItemModel(
+                "Streebog",
+                "https://github.com/adegtyarev/streebog",
+                "提供 Streebog 系列哈希算法的实现"),
+            new GenericItemModel(
+                "XKCP",
+                "https://github.com/XKCP/XKCP",
+                "提供 SHA3 系列哈希算法的实现"),
+            new GenericItemModel(
+                "xxHash",
+                "https://github.com/Cyan4973/xxHash",
+                "提供 XXH 系列哈希算法的实现"),
+            new GenericItemModel(
                 "CommandLine",
                 "https://github.com/commandlineparser/commandline",
                 "用于解析命令行参数"),
@@ -57,57 +97,17 @@ namespace HashCalculator
                 "https://www.newtonsoft.com/json",
                 "用于读取和保存本软件的相关配置文件"),
             new GenericItemModel(
-                "Tiny-Json",
+                "tiny-json",
                 "https://github.com/rafagafe/tiny-json",
                 "用于读取和保存外壳扩展的相关配置文件"),
             new GenericItemModel(
                 "WindowsAPICodePack",
                 "https://github.com/aybe/Windows-API-Code-Pack-1.1",
-                "用于打开文件/文件夹选择对话框"),
+                "用于调用系统接口打开文件/文件夹选择对话框"),
             new GenericItemModel(
                 "XamlAnimatedGif",
                 "https://github.com/XamlAnimatedGif/XamlAnimatedGif",
-                "用于在 UI 界面上显示动画"),
-            new GenericItemModel(
-                "BLAKE2",
-                "https://github.com/BLAKE2/BLAKE2",
-                "BLAKE2 系列哈希算法的 C 实现来源"),
-            new GenericItemModel(
-                "BLAKE3",
-                "https://github.com/BLAKE3-team/BLAKE3",
-                "BLAKE3 系列哈希算法的 C 实现来源"),
-            new GenericItemModel(
-                "CRC32",
-                "https://github.com/stbrumme/crc32",
-                "CRC32 哈希算法的 C 实现来源"),
-            new GenericItemModel(
-                "GmSSL",
-                "https://github.com/guanzhi/GmSSL",
-                "SM3 哈希算法的 C 实现来源"),
-            new GenericItemModel(
-                "OpenHashTab",
-                "https://github.com/namazso/OpenHashTab",
-                "CRC64 哈希算法的 C 实现来源"),
-            new GenericItemModel(
-                "QuickXorHash",
-                "https://github.com/namazso/QuickXorHash",
-                "QuickXor 哈希算法的 C 实现来源"),
-            new GenericItemModel(
-                "RHash",
-                "https://github.com/rhash/RHash",
-                "eD2k/Has160/MD4/RipeMD160/SHA224/Whirlpool 算法的 C 实现来源"),
-            new GenericItemModel(
-                "Streebog",
-                "https://github.com/adegtyarev/streebog",
-                "Streebog 系列算法的 C 实现来源"),
-            new GenericItemModel(
-                "XKCP",
-                "https://github.com/XKCP/XKCP",
-                "SHA3 系列哈希算法的 C 实现来源"),
-            new GenericItemModel(
-                "XxHash",
-                "https://github.com/Cyan4973/xxHash",
-                "XxHash 系列哈希算法的 C 实现来源"),
+                "用于在图形用户界面上显示动态图片"),
         };
     }
 }
