5cce2227934ac46977416370296b8dfcfd6d7625
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2024-04-18 13:18:03 +0800

    更新 Info 类

--- HashCalculator/Settings/Info.cs
+++ HashCalculator/Settings/Info.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Reflection;
-using System.Windows;
 
 namespace HashCalculator
 {
     internal static class Info
     {
-        private static readonly AssemblyName assembly = Application.ResourceAssembly.GetName();
-        private static readonly Version v = assembly.Version;
+        private static readonly AssemblyName executingAssembly =
+            Assembly.GetExecutingAssembly().GetName();
+        private static readonly Version v = executingAssembly.Version;
 
         public const string AppGuid = "{8CD5CD37-5AA1-492A-A208-7E9BBDDC88CC}";
         public const string ProcIdGuid = "{DA4EEDE8-5767-472F-9A40-1B90D7097953}";
         public const string MappedGuid = "{EF36065E-10F3-4ABA-A66F-E68A42A8DA7D}";
         public const string TypeLibGuid = "{18D6B7F2-F466-481F-ADFC-849B5F9FBD0B}";
         public const string ShlExtGuid = "{DE95CAC8-90D1-4C55-B81D-D7F6D527606C}";
-        public static readonly string AppName = assembly.Name;
+        public static readonly string AppName = executingAssembly.Name;
         public static readonly string Ver = $"{v.Major}.{v.Minor}.{v.Build}";
         public const string Title = "哈希值批量计算器";
         public const string Author = "hrpzcf";
