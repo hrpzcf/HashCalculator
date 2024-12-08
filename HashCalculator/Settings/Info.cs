6dd0b3624b210b6895a0225e28d419dc2acdaf76
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2024-12-08 11:38:51 +0800

    修改版本号生成方式和显示样式

--- HashCalculator/Settings/Info.cs
+++ HashCalculator/Settings/Info.cs
@@ -16,7 +16,7 @@ namespace HashCalculator
         public const string RegGuidComputeHash = "{DE95CAC8-90D1-4C55-B81D-D7F6D527606C}";
         public const string RegGuidAsCheckList = "{50B22DF9-3FF8-428E-900F-F6EE89F1A18B}";
         public static readonly string AppName = executingAssembly.Name;
-        public static readonly string Ver = $"{v.Major}.{v.Minor}.{v.Build}";
+        public static readonly string Ver = $"{v.Major}.{v.Minor}.{v.Build}{(v.Revision > 0 ? $"-preview{v.Revision}" : "")}";
         public const string Title = "哈希值批量计算器";
         public const string Author = "hrpzcf";
         public const string Published = "www.52pojie.cn";
