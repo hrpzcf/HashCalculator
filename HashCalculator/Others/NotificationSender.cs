5883c834e452d946b4fd137c310f367a8bf49405
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2025-11-30 23:48:39 +0800

    【常规设置】增加设置项【使用非全局的 Growl 信息通知】并实现功能

---
+++ HashCalculator/Others/NotificationSender.cs
@@ -0,0 +1,43 @@
+﻿using HandyControl.Controls;
+
+namespace HashCalculator
+{
+    internal static class NotificationSender
+    {
+        public static void GrowlError(string message)
+        {
+            if (Settings.Current.SendNonGlobalGrowlNotifications)
+            {
+                Growl.Error(message);
+            }
+            else
+            {
+                Growl.ErrorGlobal(message);
+            }
+        }
+
+        public static void GrowlWarning(string message)
+        {
+            if (Settings.Current.SendNonGlobalGrowlNotifications)
+            {
+                Growl.Warning(message);
+            }
+            else
+            {
+                Growl.WarningGlobal(message);
+            }
+        }
+
+        public static void GrowlSuccess(string message)
+        {
+            if (Settings.Current.SendNonGlobalGrowlNotifications)
+            {
+                Growl.Success(message);
+            }
+            else
+            {
+                Growl.SuccessGlobal(message);
+            }
+        }
+    }
+}
