860786003aad3f7c2e114379b3282d43af2bd963
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2026-03-14 22:23:04 +0800

    给 HashCalculator 的子命令添加描述。

--- HashCalculator/Others/CmdOptions.cs
+++ HashCalculator/Others/CmdOptions.cs
@@ -5,12 +5,11 @@ namespace HashCalculator
 {
     internal interface IOptions
     {
-        [Option('a', "algo",
-            HelpText = "要使用的哈希算法名称（名称中的横杠替换为下划线），如：SHA_1")]
+        [Option('a', "algo", HelpText = "要使用的哈希算法名称（名称中的横杠替换为下划线），如：SHA_1")]
         string Algos { get; set; }
     }
 
-    [Verb("verify")]
+    [Verb("verify", HelpText = "用预期的哈希值清单来校验目标文件的哈希值是否与预期相符。")]
     internal class VerifyHash : IOptions
     {
         public const string Verb = "verify";
@@ -22,7 +21,7 @@ namespace HashCalculator
         public string ChecklistPath { get; set; }
     }
 
-    [Verb("compute")]
+    [Verb("compute", HelpText = "用指定算法计算目标文件、文件夹内的文件的哈希值。")]
     internal class ComputeHash : IOptions
     {
         public const string Verb = "compute";
@@ -33,7 +32,7 @@ namespace HashCalculator
         public IEnumerable<string> FilePaths { get; set; }
     }
 
-    [Verb("shell")]
+    [Verb("shell", HelpText = "安装或卸载 HashCalculator 的系统右键菜单。")]
     internal class ShellInstallation : IOptions
     {
         // 本类实现 IOptions 接口并没有实际用处
