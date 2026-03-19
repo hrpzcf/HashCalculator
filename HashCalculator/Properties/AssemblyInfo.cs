7f4b57f8f50f8a78988b508ef9a7dfbec16b921f
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2026-03-19 16:42:24 +0800

    版本：6.2.1

--- HashCalculator/Properties/AssemblyInfo.cs
+++ HashCalculator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HashCalculator")]
 [assembly: AssemblyProduct("HashCalculator")]
-[assembly: AssemblyCopyright("Copyright (C) 2022~2025 hrpzcf")]
+[assembly: AssemblyCopyright("Copyright (C) 2022~2026 hrpzcf")]
 [assembly: AssemblyTrademark("HashCalculator")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.2.0.0")]
-[assembly: AssemblyFileVersion("6.2.0.0")]
+[assembly: AssemblyVersion("6.2.1.0")]
+[assembly: AssemblyFileVersion("6.2.1.0")]
