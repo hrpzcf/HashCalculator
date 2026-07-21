d3ea1ecc414112b3e75e5c76718a962949a44f2b
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2026-07-21 10:47:59 +0800

    refactor: 移动异常中断反馈窗口目录位置和命名空间

---
+++ HashCalculator/Views/Windows/ExceptionWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using HashCalculator.ViewModels.Windows;
+
+namespace HashCalculator.Views.Windows;
+
+public partial class ExceptionWindow
+{
+
+    internal ExceptionWindowModel Model { get; }
+
+    public ExceptionWindow()
+    {
+        this.Model = new ExceptionWindowModel(this);
+        this.DataContext = this.Model;
+        this.InitializeComponent();
+        this.Closed += this.ExceptionWindowClosed;
+    }
+
+    private void ExceptionWindowClosed(object sender, EventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
