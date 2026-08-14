406f34b2ab30a1630e1ce9b32ee285bacefbc2f0
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2026-08-14 12:10:09 +0800

    docs(ui): 更新默认算法相关文案为算法页表述

--- HashCalculator/ViewModels/Pages/SettingsViewModel.cs
+++ HashCalculator/ViewModels/Pages/SettingsViewModel.cs
@@ -1780,7 +1780,7 @@ public class SettingsViewModel : BaseViewModel
     [JsonIgnore, XmlIgnore]
     public GenericItemModel[] AvailableFetchAlgoOptions { get; } =
     {
-        new GenericItemModel("使用默认算法中被勾选的算法", FetchAlgoOption.SELECTED),
+        new GenericItemModel("使用算法页中被勾选的算法", FetchAlgoOption.SELECTED),
         new GenericItemModel("使用被勾选且可产生相应哈希长度的算法", FetchAlgoOption.TATSAMSHDL),
         new GenericItemModel("使用所有可产生相应哈希长度的算法", FetchAlgoOption.TATMSHDL),
     };
