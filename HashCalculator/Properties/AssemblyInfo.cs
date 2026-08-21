d83095cdaea67b3b1726d4fc544228fbccb30412
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2026-08-21 20:56:21 +0800

    版本：7.0.0.3

--- HashCalculator/Properties/AssemblyInfo.cs
+++ HashCalculator/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.0.2")]
-[assembly: AssemblyFileVersion("7.0.0.2")]
+[assembly: AssemblyVersion("7.0.0.3")]
+[assembly: AssemblyFileVersion("7.0.0.3")]
 [assembly: SupportedOSPlatform("windows7.0")]
